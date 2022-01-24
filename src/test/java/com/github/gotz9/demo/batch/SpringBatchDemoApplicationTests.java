e5afd18134d2a569434c12325305187ae32c0f96
xh-gotz9 <xh353127981@gmail.com>
2022-01-24 23:12:32 +0800

    init by Spring Initializer
    - Spring batch
    - H2 database
    - Spring web

---
+++ src/test/java/com/github/gotz9/demo/batch/SpringBatchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gotz9.demo.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
