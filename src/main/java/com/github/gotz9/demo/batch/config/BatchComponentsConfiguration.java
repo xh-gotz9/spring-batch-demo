2e702aa18cc974824cf6753e5ac8e47534eea879
xh-gotz9 <xh353127981@gmail.com>
2022-01-25 01:45:58 +0800

    add multi-datasource configuration and use h2 datasource in Spring Batch

--- src/main/java/com/github/gotz9/demo/batch/config/BatchComponentsConfiguration.java
+++ src/main/java/com/github/gotz9/demo/batch/config/BatchComponentsConfiguration.java
@@ -1,12 +1,58 @@
 package com.github.gotz9.demo.batch.config;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.configuration.support.JobRegistryBeanPostProcessor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.batch.BatchDataSource;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class BatchComponentsConfiguration {
+
+    static class MultiDataSourceConfiguration {
+
+        @Bean
+        @Primary
+        @ConfigurationProperties(prefix = "spring.datasource")
+        DataSourceProperties defaultDataSourceProperties() {
+            return new DataSourceProperties();
+        }
+
+        @Bean("MySQL-DataSource")
+        @Primary
+        DataSource mysqlDataSource(DataSourceProperties properties) {
+            return properties.initializeDataSourceBuilder()
+                    .type(HikariDataSource.class)
+                    .build();
+        }
+
+        @Bean("h2-datasource-properties")
+        @ConfigurationProperties("app.datasource.h2")
+        DataSourceProperties h2DatasourceProperties() {
+            return new DataSourceProperties();
+        }
+
+        /**
+         * create a datasource marked with {@link BatchDataSource}.
+         * <p>
+         * It will be used in Spring Batch components.
+         */
+        @Bean("H2-DataSource")
+        @BatchDataSource
+        DataSource h2DataSource(@Qualifier("h2-datasource-properties") DataSourceProperties properties) {
+            return properties.initializeDataSourceBuilder()
+                    .type(HikariDataSource.class)
+                    .build();
+        }
+
+    }
 
     /**
      * auto register jobs into jobRegistry
